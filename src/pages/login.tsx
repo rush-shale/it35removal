fd3a500e9bb2806914700b0873962edf9761e69c
ronabaldoza <20221238@nbsc.edu.ph>
2025-06-02 11:03:09 +0800

    modify the code

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,72 +1,34 @@
 import { 
-  IonAvatar,
-  IonButton,
-  IonContent, 
-  IonHeader, 
-  IonInput, 
-  IonItem, 
-  IonPage, 
-  IonTitle, 
-  IonToolbar, 
-  useIonRouter
-} from '@ionic/react';
-
-const Login: React.FC = () => {
-  const navigation = useIonRouter();
-
-  const doLogin = () => {
-      navigation.push('/it35-lab/app', 'forward', 'replace');
+    IonButton,
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar, 
+      useIonRouter
+  } from '@ionic/react';
+  
+  const Login: React.FC = () => {
+    const navigation = useIonRouter();
+    const doLogin = () => {
+        navigation.push('/it35-lab/app','forward','replace');
+    }
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Login</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className='ion-padding'>
+            <IonButton onClick={() => doLogin()} expand="full">
+                Login
+            </IonButton>
+        </IonContent>
+      </IonPage>
+    );
   };
   
-  const goToSignUp = () => {
-      navigation.push('/signup', 'forward', 'replace');
-  };
-  
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <IonContent className='ion-padding'>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-          
-          {/* Avatar */}
-          <IonAvatar style={{ width: '80px', height: '80px', marginBottom: '20px' }}>
-            <img alt="User Avatar" src="https://ionicframework.com/docs/img/demos/avatar.svg" />
-          </IonAvatar>
-         
-          {/* Email Input */}
-          <IonItem style={{ marginBottom: '15px', width: '80%' }}>
-            <IonInput label="Email:" placeholder="Enter your email" />
-          </IonItem>
-
-          {/* Password Input */}
-          <IonItem style={{ marginBottom: '15px', width: '80%' }}>
-            <IonInput type="password" label="Password:" placeholder="Enter your password" />
-          </IonItem>
-
-          {/* Buttons */}
-          <IonButton onClick={doLogin} expand="block" style={{ width: '80%', height: '50px', fontWeight: 'bold' }}>
-            Login
-          </IonButton>
-          
-          <IonButton 
-            onClick={goToSignUp} 
-            expand="block" 
-            fill="outline" 
-            color="secondary" 
-            style={{ width: '80%', height: '50px', fontWeight: 'bold', marginTop: '10px' }}
-          >
-            Don't have an account? Register here
-          </IonButton>
-
-        </div>
-      </IonContent>
-    </IonPage>
-  );
-};
-
-export default Login;
+  export default Login; 
--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,22 +1,24 @@
 
-import {
-  IonButton,
-  IonContent,
-  IonHeader,
-  IonIcon,
-  IonItem,
-  IonMenu,
-  IonMenuToggle,
-  IonPage,
-  IonRouterOutlet,
-  IonSplitPane,
-  IonTitle,
-  IonToolbar,
+import { 
+    IonButton,
+    IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonIcon, 
+    IonItem, 
+    IonMenu, 
+    IonMenuButton, 
+    IonMenuToggle, 
+    IonPage, 
+    IonRouterOutlet, 
+    IonTitle, 
+    IonToolbar 
 } from '@ionic/react';
 import { homeOutline, logOutOutline, rocketOutline } from 'ionicons/icons';
 import { Redirect, Route } from 'react-router';
 import Home from './Home';
 import About from './about';
+import Details from './details';
 
 const Menu: React.FC = () => {
   const path = [
@@ -25,45 +27,43 @@ const Menu: React.FC = () => {
   ];
 
   return (
-    <IonPage>
-      <IonSplitPane contentId="main">
-        <IonMenu contentId="main">
-          <IonHeader>
-            <IonToolbar>
-              <IonTitle>Menu</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent>
-            {path.map((item, index) => (
-              <IonMenuToggle key={index} auto-hide="false">
-                <IonItem routerLink={item.url} routerDirection="forward">
-                  <IonIcon icon={item.icon} slot="start" />
-                  {item.name}
-                </IonItem>
-              </IonMenuToggle>
-            ))}
-            <IonButton
-              routerLink="/it35-lab"
-              routerDirection="back"
-              expand="full"
-              className="ion-margin-top"
-            >
-              <IonIcon icon={logOutOutline} slot="start" />
-              Logout
-            </IonButton>
-          </IonContent>
-        </IonMenu>
+    <>
+      <IonMenu contentId="main" side="start">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          {path.map((item, index) => (
+            <IonMenuToggle key={index} autoHide={false}>
+              <IonItem routerLink={item.url} routerDirection="forward">
+                <IonIcon icon={item.icon} slot="start"></IonIcon>
+                {item.name}
+              </IonItem>
+            </IonMenuToggle>
+          ))}
 
-        <IonRouterOutlet id="main">
+          {/* Logout Button */}
+          <IonButton routerLink="/it35-lab" routerDirection="back" expand="full">
+            <IonIcon icon={logOutOutline} slot="start"></IonIcon>
+            Logout
+          </IonButton>
+        </IonContent>
+      </IonMenu>
+
+      <IonPage id="main">
+        <IonRouterOutlet>
           <Route exact path="/it35-lab/app/home" component={Home} />
           <Route exact path="/it35-lab/app/about" component={About} />
+          <Route exact path="/it35-lab/app/home/details" component={Details} />
 
           <Route exact path="/it35-lab/app">
             <Redirect to="/it35-lab/app/home" />
           </Route>
         </IonRouterOutlet>
-      </IonSplitPane>
-    </IonPage>
+      </IonPage>
+    </>
   );
 };
 
